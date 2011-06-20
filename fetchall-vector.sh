8e88959dc218229bd358a194d93ad2b8ebb84fc0
straup <straup@gmail.com>
2011-06-20 11:00:24 -0700

    initial commit

---
+++ fetchall-vector.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget http://www.naturalearthdata.com/download/10m/cultural/10m-cultural.zip
+wget http://www.naturalearthdata.com/download/10m/physical/10m-physical.zip
+
+wget http://www.naturalearthdata.com/download/50m/cultural/50m-cultural.zip
+wget http://www.naturalearthdata.com/download/50m/physical/50m-physical.zip
+
+wget http://www.naturalearthdata.com/download/110m/cultural/110m-cultural.zip
+wget http://www.naturalearthdata.com/download/110m/physical/110m-physical.zip
+
