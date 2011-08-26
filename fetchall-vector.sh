c9d19996cfe2b8bf8750d1650afa42eb019ed252
straup <straup@gmail.com>
2011-08-26 19:50:56 +0200

    account for super weird URLs...

--- fetchall-vector.sh
+++ fetchall-vector.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-wget http://www.naturalearthdata.com/download/10m/cultural/10m-cultural.zip
-wget http://www.naturalearthdata.com/download/10m/physical/10m-physical.zip
+# Yes, those URLs look super weird. Because they are.
+# But they work.
 
-wget http://www.naturalearthdata.com/download/50m/cultural/50m-cultural.zip
-wget http://www.naturalearthdata.com/download/50m/physical/50m-physical.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m-physical.zip
 
-wget http://www.naturalearthdata.com/download/110m/cultural/110m-cultural.zip
-wget http://www.naturalearthdata.com/download/110m/physical/110m-physical.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m-cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/50m-physical.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m-cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/physical/110m-physical.zip
 
