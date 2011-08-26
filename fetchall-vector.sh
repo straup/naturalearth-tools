dc742e4c90b0c439328adf2d25ba1db4695ffc51
straup <straup@gmail.com>
2011-08-26 19:54:52 +0200

    added a note about bundle ne vector download

--- fetchall-vector.sh
+++ fetchall-vector.sh
@@ -3,6 +3,9 @@
 # Yes, those URLs look super weird. Because they are.
 # But they work.
 
+# Packages are also available in a single bundle here:
+# http://www.nacis.org/naturalearth/packages/natural_earth_vector.zip
+
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-cultural.zip
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m-physical.zip
 
